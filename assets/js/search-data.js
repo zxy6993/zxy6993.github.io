e085af39137fde8f09d90a4402d94617a63821b1
zxy6993 <68778689+zxy6993@users.noreply.github.com>
2026-02-13 16:33:35 +0000

    Deploying to gh-pages from @ zxy6993/zxy6993.github.io@2f13c9472316596aad4ed4672d6a33264b8c28f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -476,35 +476,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("mailto:%78%69%79%75%61%6E.%7A%68%61%6F@%73%65%75.%65%64%75.%63%6E", "_blank");
         },
       },{
       id: 'light-theme',
