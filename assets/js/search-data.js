dc9c952c5cdc171f5fd5692e193b2adcf28c38b1
zxy6993 <68778689+zxy6993@users.noreply.github.com>
2026-02-13 10:04:07 +0000

    Deploying to gh-pages from @ zxy6993/zxy6993.github.io@11eecb04f30155b616cb3dc066817fbc17faadba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Recent publications.",
+          description: "Recent Publications",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
