70007c9546befe1c7db4e04b0166c911dc36afbc
zxy6993 <68778689+zxy6993@users.noreply.github.com>
2026-02-13 03:29:25 +0000

    Deploying to gh-pages from @ zxy6993/zxy6993.github.io@f0f95fafa0712cf2de5034f6c1bb803cff4e6dd1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
+          title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
